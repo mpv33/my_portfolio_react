bf404d8df5076289ea792ee7fa679e242d13aae6
mateshwari verma <46149432+mpv33@users.noreply.github.com>
2021-02-11 14:42:06 +0530

    build

---
+++ build/precache-manifest.2d5fe5405c9cc86223983a190358667d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dba951834390a864820b561d1d0cb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3f745ef3186fedb6c1",
+    "url": "/static/css/main.4dc6bc38.chunk.css"
+  },
+  {
+    "revision": "925bbc72392c27ef998f",
+    "url": "/static/js/2.30e0275f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.30e0275f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3f745ef3186fedb6c1",
+    "url": "/static/js/main.8f0e3efc.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "2942bfabb3d05332b66eb128e0842cff",
+    "url": "/static/media/resume.2942bfab.pdf"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/static/media/tie.4a04f9f0.svg"
+  }
+]);
